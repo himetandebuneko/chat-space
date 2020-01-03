0fa3b884e3f8f08f45201badcd94230f52ad3ed5
himetandebuneko <ryunosukeman@gmail.com>
2020-01-03 16:02:21 +0900

    automatic-updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,10 @@
 $(function(){
+  last_message_id = $('.message:last').data("message-id");
+  
     function buildHTML(message){
       if ( message.image ) {
         var html =
-          `<div class="chat-main__message-list__message">
+          `<div class="chat-main__message-list__message" data-message-id= message.id>
             <div class="message">
               <div class="message__user-name">
                 ${message.user_name}
@@ -18,10 +20,10 @@ $(function(){
             </div>
             <img src=${message.image} ></img>
           </div>`
-      return html;
+        return html;
       }else{
         var html =
-          `<div class="chat-main__message-list__message">
+          `<div class="chat-main__message-list__message" data-message-id= message.id>
             <div class="message">
               <div class="message__user-name">
                 ${message.user_name}
@@ -36,7 +38,7 @@ $(function(){
               </p>
             </div>
           </div>`
-          return html;
+        return html;
       };
     }
   $('#new_message').on('submit', function(e){
@@ -61,4 +63,32 @@ $(function(){
         alert("メッセージ送信に失敗しました")
     })
   })
+
+  var reloadMessages = function() {
+    last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+     if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.chat-main__message-list').append(insertHTML);
+      $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      $("#new_message")[0].reset();
+      $(".message-form").prop("disabled",false);
+     }
+    })
+    .fail(function() {
+      console.log('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+   setInterval(reloadMessages, 7000);
+  }
 });
