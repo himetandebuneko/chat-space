9d48f68955a1750bebedfa21492b1d15235cb166
himetandebuneko <ryunosukeman@gmail.com>
2020-01-03 16:36:56 +0900

    resolve

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -84,9 +84,7 @@ $(function(){
       $(".message-form").prop("disabled",false);
      }
     })
-    .fail(function() {
-      console.log('error');
-    });
+    
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
    setInterval(reloadMessages, 7000);
